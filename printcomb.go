3aebd7105cc0386e72042d9c2ad82d1ebfde52ed
jasonasante <jason.asante@outlook.com>
2021-08-11 17:08:27 +0100

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,9 +3,9 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i = 48; i <= 55; i++ {
-		for j = i + 1; j <= 56; j++ {
-			for k = j + 1; k <= 57; k++ {
+	for i := 48; i <= 55; i++ {
+		for j := i + 1; j <= 56; j++ {
+			for k := j + 1; k <= 57; k++ {
 				z01.PrintRune(rune(i))
 				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))
