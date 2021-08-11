613c33dc30ecd7be5f925d7369b94b8ee9eb7f54
jasonasante <jason.asante@outlook.com>
2021-08-11 16:00:44 +0100

    isnegative

--- isnegative.go
+++ isnegative.go
@@ -1,13 +1,12 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
-func IsNegative (nb int){
-	if nb <0 {
+func IsNegative(nb int) {
+	if nb < 0 {
 		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
 	}
-		else {
-			z01.PrintRune('F')
-		}
-		z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
