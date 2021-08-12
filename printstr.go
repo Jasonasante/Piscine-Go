1de7df754819fda83470b8f65c63954e5ed16049
jasonasante <jason.asante@outlook.com>
2021-08-12 19:48:39 +0100

    PrintStr

--- printstr.go
+++ printstr.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
 	schangeable := []byte(s)
-	for i := range schangeable {
-		z01.PrintRune(rune(i))
+	for index, i := range schangeable {
+		z01.PrintRune(rune(%v))
 	}
 }
