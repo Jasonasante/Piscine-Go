70a966a381993ab473b71e6ab273f6fa44737cf4
jasonasante <jason.asante@outlook.com>
2021-08-16 18:25:22 +0100

    Fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -7,10 +7,9 @@ func Fibonacci(index int) int {
 	if index == 0 {
 		return 0
 	}
-	if index ==1 {
+	if index == 1 {
 		return 1
-	}else{
+	} else {
 		return Fibonacci(index-1) + Fibonacci(index-2)
 	}
-	return index
 }
