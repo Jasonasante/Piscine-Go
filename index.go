fc216c13bc0c63ca0b63eb9e7a42541184614379
jasonasante <jason.asante@outlook.com>
2021-08-18 18:07:16 +0100

    Index

--- index.go
+++ index.go
@@ -4,8 +4,12 @@ func Index(s string, toFind string) int {
 	string := []rune(s)
 	substring := []rune(toFind)
 	counter := 0
-	if len(toFind) > len(s) {
+
+	if len(toFind) == 0 {
 		return 0
+	}
+	if len(toFind) > len(s) {
+		return -1
 	} else {
 		for i := 0; i <= len(s)-len(toFind); i++ {
 			for j := 0; j < len(toFind); j++ {
