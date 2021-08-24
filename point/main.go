44ae791a3728a52b44ccb08cab8f5902dbd384d4
jasonasante <jason.asante@outlook.com>
2021-08-24 19:57:15 +0100

    Point

--- point/main.go
+++ point/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
 func main() {
-	x := 42
-	y := 21
-	fmt.Printf("x = %v, y = %v\n", x, y)
+	r := "x = 42, y = 21"
+	printStr(r)
 }
