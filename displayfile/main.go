36e8e83ccb47e134ec6b04c6c5a7f1730ae24916
jasonasante <jason.asante@outlook.com>
2021-08-24 21:46:10 +0100

    Main

--- displayfile/main.go
+++ displayfile/main.go
@@ -8,17 +8,17 @@ import (
 
 func main() {
 	file := "quest8.txt"
-	if len(os.Args) < 2 {
+	if len(os.Args) < 1 {
 		fmt.Println("File name missing")
 		return
 	}
-	if len(os.Args) > 2 {
+	if len(os.Args) > 1 {
 		fmt.Println("Too many arguments")
 		return
 	}
 	content, err := ioutil.ReadFile(file)
 	if err != nil {
-		fmt.Printf("the mistke is: %v\n", err.Error())
+		fmt.Printf("the mistake is: %v\n", err.Error())
 		return
 	}
 	fmt.Println(string(content))
