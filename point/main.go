239804aec4320fcf0cae1e2bb708697728fae154
jasonasante <jason.asante@outlook.com>
2021-08-24 20:11:40 +0100

    Point

--- point/main.go
+++ point/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
+	for _, i := range s {
+		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
 }
