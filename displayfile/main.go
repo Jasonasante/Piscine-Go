c4a4f7db88df6418badfda781fa71d0dd743a966
jasonasante <jason.asante@outlook.com>
2021-08-24 21:25:15 +0100

    DisplayFile

---
+++ displayfile/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	file := "quest8.txt"
+	if len(os.Args) < 2 {
+		fmt.Println("File name missing")
+		return
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(string(content))
+}
