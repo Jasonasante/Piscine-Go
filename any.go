83999907e4425894cf09b00c3d8a747a60a15ace
jasonasante <jason.asante@outlook.com>
2021-08-25 18:21:42 +0100

    Any

--- any.go
+++ any.go
@@ -5,6 +5,11 @@ func Any(f func(string) bool, a []string) bool {
 	for _, e := range a {
 		f(e)
 		result = f(e)
+		if result {
+			return false
+		} else {
+			return true
+		}
 	}
 	return result
 }
