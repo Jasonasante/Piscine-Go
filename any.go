3fc18d76e9fa058d7229db4aa1d3d0532f3a1ff5
jasonasante <jason.asante@outlook.com>
2021-08-25 18:35:34 +0100

    Any

--- any.go
+++ any.go
@@ -3,11 +3,8 @@ package piscine
 func Any(f func(string) bool, a []string) bool {
 	var result bool
 	for _, e := range a {
-		f(e)
 		result = f(e)
-		if result {
-			return false
-		} else {
+		if result == true {
 			return true
 		}
 	}
