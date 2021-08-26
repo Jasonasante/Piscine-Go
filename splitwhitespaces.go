68ca3274738a1eb3234f8436b57b2e4501738095
jasonasante <jason.asante@outlook.com>
2021-08-26 12:51:35 +0100

    SplitWhiteSpaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,6 +1,18 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	tab := []string{s}
-	return tab
+	var result []string
+	divider := ""
+	for _, value := range s {
+		if value != ' ' && value != '\n' && value != '\t' {
+			divider += string(value)
+		} else {
+			if divider != "" {
+				result = append(result, divider)
+				divider = ""
+			}
+		}
+	}
+	result = append(result, divider)
+	return result
 }
