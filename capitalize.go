8a933d8b86ca5cf4307d509ab05afa5a34bb7722
jasonasante <jason.asante@outlook.com>
2021-08-26 15:08:35 +0100

    Capitalize

---
+++ capitalize.go
@@ -0,0 +1,20 @@
+package piscine
+
+func Capitalize(s string) string {
+	r := []rune(s)
+	var cap []rune
+	for i := 0; i < len(r)-1; i++ {
+		if r[0] >= 'a' && r[0] <= 'z' {
+			r[0] = r[0] - 32
+			cap = append(cap, r[0])
+		}
+		if r[i] < 48 || r[i] > 57 && r[i] < 65 || r[i] > 90 && r[i] < 97 || r[i] > 122 {
+			if r[i+1] >= 'a' && r[i+1] <= 'z' {
+				r[i+1] = r[i+1] - 32
+			}
+		}
+		cap = append(cap, r[i])
+	}
+	cap = append(cap, r[(len(r)-1)])
+	return string(cap)
+}
